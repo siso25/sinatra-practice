2725eab4c8b9f22a708e51e340e4389877eb1cf1
siso <siso25@zoho.com>
2021-02-01 22:11:43 +0900

    ルーティングの設定
    ルーティングを設定。ビューファイルの追加。

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get "/hello" do
-  "hello"
+get "/memos" do
+  erb :index
+end
+
+post "/memos" do
+end
+
+get "/memos/new" do
+  erb :new
+end
+
+get "/memos/:memo_id" do
+  erb :show
+end
+
+patch "/memos/:memo_id" do
+end
+
+delete "/memos/:memo_id" do
+end
+
+get "/memos/:memo_id/edit" do
+  erb :edit
 end
